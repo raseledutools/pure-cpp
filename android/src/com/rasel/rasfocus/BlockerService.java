7c3f8fa4e35a15f2b75bef3b764c851292dbb22f
raseledutools <bhmrasel60@gmail.com>
2026-04-02 21:51:07 +0600

    Update BlockerService.java

--- android/src/com/rasel/rasfocus/BlockerService.java
+++ android/src/com/rasel/rasfocus/BlockerService.java
@@ -1,1 +1,36 @@
+package com.rasel.rasfocus;
 
+import android.content.Context;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Intent;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class BlockerService {
+    public static void checkAndBlock(Context context) {
+        UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+        long time = System.currentTimeMillis();
+        // শেষ ১০ সেকেন্ডের ডাটা চেক করবে
+        List<UsageStats> appList = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 10, time);
+        
+        if (appList != null && appList.size() > 0) {
+            SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+            for (UsageStats usageStats : appList) {
+                mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+            }
+            if (!mySortedMap.isEmpty()) {
+                String currentApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                
+                // এখানে তুমি তোমার ব্লক লিস্ট সেট করবে
+                if (currentApp.equals("com.facebook.katana") || currentApp.equals("com.android.chrome")) {
+                    Intent startMain = new Intent(Intent.ACTION_MAIN);
+                    startMain.addCategory(Intent.CATEGORY_HOME);
+                    startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    context.startActivity(startMain);
+                }
+            }
+        }
+    }
+}
